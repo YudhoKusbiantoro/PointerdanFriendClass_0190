a32d726c13d13d67a4d02f861346f6667533e0dd
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2024-05-07 11:36:08 +0700

    Membuat class mahasiswa bersifat publik pada pointer 1

--- PointerSatu/PointerSatu.cpp
+++ PointerSatu/PointerSatu.cpp
@@ -1,5 +1,14 @@
-// PointerSatu.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
 using namespace std;
+
+class mahasiswa{
+	public:
+	int nim;
+	void showNim() {
+		cout << " No Induk = " << nim << endl;
+
+	}
+};
+
